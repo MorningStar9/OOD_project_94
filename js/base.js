43658375195a764a095cb566de2495f33f24aa46
MorningStar9 <k.edalat@yahoo.com>
2015-05-15 21:48:57 +0430

    manager nav

---
+++ js/base.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kasra on 5/15/2015.
+ */
+
+$('#navbar-search-form .dropdown-menu li a').each(function(id, elem){
+	elem.onclick = function(event){
+		$('#navbar-subject-chooser').html(elem.text + '<span class="caret"></span>');
+	}
+});
